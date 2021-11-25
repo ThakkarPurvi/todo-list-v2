391715ecc98c8814c79e39993135e28404c7b97b
Ubuntu <purvi@DevJenkins.l2qd0vbrbuhuhgbbbu1dozytpd.ax.internal.cloudapp.net>
2021-11-25 18:39:11 +0000

    fixing deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,4 +2,7 @@
 
 echo "Deploy Stage"
 
-ssh jenkins@DevJenkins docker stack deploy --compose-file docker-compose.yaml todo-app
+scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+ssh jenkins@swarm-manager \
+    DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    docker stack deploy --compose-file docker-compose.yaml todo-app
