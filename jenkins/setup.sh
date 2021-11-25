9077fa60da5e4d1e92924921889735cd8956d39a
Ubuntu <purvi@DevJenkins.l2qd0vbrbuhuhgbbbu1dozytpd.ax.internal.cloudapp.net>
2021-11-25 14:40:37 +0000

    saving changes

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Setup Stage"
+
+# install docker 
+sudo apt-get update
+sudo apt install curl -y
+curl https://get.docker.com | sudo bash
+
+# install docker compose
+
+
+# docker login
+
