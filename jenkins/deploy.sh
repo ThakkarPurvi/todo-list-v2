8a643b936a364d3988639611a9f6b823852b62d3
Ubuntu <purvi@DevJenkins.l2qd0vbrbuhuhgbbbu1dozytpd.ax.internal.cloudapp.net>
2021-11-26 15:04:17 +0000

    fixing api and schema correctly

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -3,7 +3,9 @@
 echo "Deploy stage"
 
 scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+
 ssh jenkins@swarm-manager \
+    CREATE_SCHEMA=$CREATE_SCHEMA \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
     DATABASE_URI=$DATABASE_URI \
     docker stack deploy --compose-file docker-compose.yaml todo-app
